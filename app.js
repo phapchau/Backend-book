b589d5620d0ee3b50b24e522474e1c77fc4011fa
phapchau <phapx105303@gmail.com>
2024-09-06 22:01:11 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const cors =  require("cors");
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 const contactRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) =>{
+    //Code ở đây sẽ chạy khi không có route được định nghĩa nào
+    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+
+//define error-handling middeware last, after other app.use() and router calls
+app.use((err,req, res, next) =>{
+    //Middleware xử lý lỗi tập trung.
+    //Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
+    return res.status(error.statusCode || 500 ).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 app use("/api/contacts", contactsRouter);
 
